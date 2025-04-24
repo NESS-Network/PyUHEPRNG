9039965a29f505383a1eb3b9ce274c3a3375c718
PrivateNess.Network <74679402+NESS-Network@users.noreply.github.com>
2025-04-24 01:54:49 -0400

    Add files via upload
    setup.py for better interoperability

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyuheprng",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A pseudorandom number generator for privateness network tools",
+    author="Ness Network",
+    url="https://github.com/ness-network/pyuheprng",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
